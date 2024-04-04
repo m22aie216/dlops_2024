a6a675741c5684303ad236d6a356a7a2a7dceb06
m22aie216 <142084927+m22aie216@users.noreply.github.com>
2024-04-04 20:12:30 +0530

    random variables

--- activattion.py
+++ activattion.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #feature -1
+random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+
 # Sigmoid Activation Function
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
