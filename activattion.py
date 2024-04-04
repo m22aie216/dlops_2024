cba5078072389c455815c3a001b9528dd0d145ab
m22aie216 <142084927+m22aie216@users.noreply.github.com>
2024-04-04 18:44:56 +0530

    feature-1 commit

---
+++ activattion.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Sigmoid Activation Function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# ReLU Activation Function
+def relu(x):
+    return np.maximum(0, x)
+
+# Leaky ReLU Activation Function
+def leaky_relu(x, alpha=0.01):
+    return np.where(x > 0, x, alpha * x)
+
+# Tanh Activation Function
+def tanh(x):
+    return np.tanh(x)
+
+# Generate x values
+x = np.linspace(-10, 10, 1000)
+
+# Plot the activation functions
+plt.figure(figsize=(10, 6))
+
+plt.subplot(2, 2, 1)
+plt.plot(x, sigmoid(x), label='Sigmoid')
+plt.title('Sigmoid Activation Function')
+plt.grid()
+
+plt.subplot(2, 2, 2)
+plt.plot(x, relu(x), label='ReLU')
+plt.title('ReLU Activation Function')
+plt.grid()
+
+plt.subplot(2, 2, 3)
+plt.plot(x, leaky_relu(x), label='Leaky ReLU')
+plt.title('Leaky ReLU Activation Function')
+plt.grid()
+
+plt.subplot(2, 2, 4)
+plt.plot(x, tanh(x), label='Tanh')
+plt.title('Tanh Activation Function')
+plt.grid()
+
+plt.tight_layout()
+plt.show()
